5d89ddebd06b9ffccc005f39ba6b38abfdb09c0c
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-15 12:55:58 +0100

    Formatting
    Formatted the program

--- my_go_programs/ex002_print_lines_regex_cl_arg.go
+++ my_go_programs/ex002_print_lines_regex_cl_arg.go
@@ -1,51 +1,48 @@
 package main
 
-
 // https://gobyexample.com/command-line-arguments
 // Run with: go run ex002_print_lines_regex_cl_arg.go "\brs429358\b"
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
 
-    import (
-        "bufio"
-        "fmt"
-        "log"
-        "os"
-        "regexp"
-    )
-
-    func main() {
-	    regex := os.Args[1]
-	    	homeDir := getHomeDir()
+func main() {
+	regex := os.Args[1]
+	homeDir := getHomeDir()
 	pathToFile := fmt.Sprintf("%s/big_files/1000GENOMES-phase_3.vcf", homeDir)
 	file, err := os.Open(pathToFile)
-        if err != nil {
-            log.Fatal(err)
-        }
-        defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-        scanner := bufio.NewScanner(file)
-        r, err := regexp.Compile(regex) 
+	scanner := bufio.NewScanner(file)
+	r, err := regexp.Compile(regex)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        for scanner.Scan() {
-            if r.MatchString(scanner.Text()) {
-                fmt.Println(scanner.Text())
-            }
-        }
+	for scanner.Scan() {
+		if r.MatchString(scanner.Text()) {
+			fmt.Println(scanner.Text())
+		}
+	}
 
-        if err := scanner.Err(); err != nil {
-            log.Fatal(err)
-        }
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
 
-    func getHomeDir() string {
+func getHomeDir() string {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
 	return homeDir
 }
-

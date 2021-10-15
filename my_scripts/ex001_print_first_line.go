7d6c77c50dce8ec94863780c8657b8b519387afb
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-15 11:47:17 +0100

    Added my own scripts folder
    Added my own scripts folder

---
+++ my_scripts/ex001_print_first_line.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"bufio"
+)
+
+func main() {
+	homeDir := getHomeDir()
+	pathToFile := fmt.Sprintf("%s/big_files/1000GENOMES-phase_3.vcf", homeDir)
+	file, err := os.Open(pathToFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var firstLine string
+	for scanner.Scan() {
+		firstLine = scanner.Text()
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(firstLine)
+}
+
+func getHomeDir() string {
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        log.Fatal( err )
+    }
+    return homeDir
+}

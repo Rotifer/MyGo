354ea543fa34cb67da5563b6601fd6ce43f0fce8
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-19 11:40:54 +0100

    formatted program
    Ran fmt

--- mastering_go_book/ch01/cla_listdir.go
+++ mastering_go_book/ch01/cla_listdir.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
 	var path string
-    arguments := os.Args
-    if len(arguments) == 1 {
-	path = "."
-    } else {
-	path = os.Args[1]
-}
+	arguments := os.Args
+	if len(arguments) == 1 {
+		path = "."
+	} else {
+		path = os.Args[1]
+	}
 
-    files, err := ioutil.ReadDir(path)
+	files, err := ioutil.ReadDir(path)
 
-    if err != nil {
+	if err != nil {
 
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
-    for _, f := range files {
-            fmt.Println(f.Name())
-    }
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }

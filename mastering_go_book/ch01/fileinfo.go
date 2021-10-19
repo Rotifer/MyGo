115b3c77f82d3df0eda39c8ece897496664823d1
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-19 17:59:35 +0100

    Formatted scripts
    Formatted scripts

--- mastering_go_book/ch01/fileinfo.go
+++ mastering_go_book/ch01/fileinfo.go
@@ -1,45 +1,45 @@
- package main
+package main
 
- import (
-         "fmt"
-         "os"
- )
+import (
+	"fmt"
+	"os"
+)
 
- func main() {
+func main() {
 
-         // can handle symbolic link, but will no follow the link
-	 filePath := os.Args[1]
-         fileInfo, err := os.Lstat(filePath)
+	// can handle symbolic link, but will no follow the link
+	filePath := os.Args[1]
+	fileInfo, err := os.Lstat(filePath)
 
-         // cannot handle symbolic link
-         //fileInfo, err := os.Lstat("file.txt")
+	// cannot handle symbolic link
+	//fileInfo, err := os.Lstat("file.txt")
 
-         if err != nil {
-                 panic(err)
-         }
+	if err != nil {
+		panic(err)
+	}
 
-         fmt.Println("Name : ", fileInfo.Name())
+	fmt.Println("Name : ", fileInfo.Name())
 
-         fmt.Println("Size : ", fileInfo.Size())
+	fmt.Println("Size : ", fileInfo.Size())
 
-         fmt.Println("Mode/permission : ", fileInfo.Mode())
+	fmt.Println("Mode/permission : ", fileInfo.Mode())
 
-         // --- check if file is a symlink
+	// --- check if file is a symlink
 
-         if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
-                 fmt.Println("File is a symbolic link")
-         }
+	if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+		fmt.Println("File is a symbolic link")
+	}
 
-         fmt.Println("Modification Time : ", fileInfo.ModTime())
+	fmt.Println("Modification Time : ", fileInfo.ModTime())
 
-         fmt.Println("Is a directory? : ", fileInfo.IsDir())
+	fmt.Println("Is a directory? : ", fileInfo.IsDir())
 
-         fmt.Println("Is a regular file? : ", fileInfo.Mode().IsRegular())
+	fmt.Println("Is a regular file? : ", fileInfo.Mode().IsRegular())
 
-         fmt.Println("Unix permission bits? : ", fileInfo.Mode().Perm())
+	fmt.Println("Unix permission bits? : ", fileInfo.Mode().Perm())
 
-         fmt.Println("Permission in string : ", fileInfo.Mode().String())
+	fmt.Println("Permission in string : ", fileInfo.Mode().String())
 
-         fmt.Println("What else underneath? : ", fileInfo.Sys())
+	fmt.Println("What else underneath? : ", fileInfo.Sys())
 
- }
+}

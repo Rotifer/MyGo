b13d7baddb922b7e34100a12ad32e9ec980589b1
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-19 18:10:36 +0100

    Formatted
    Re-formatted program

--- mastering_go_book/ch01/system_log.go
+++ mastering_go_book/ch01/system_log.go
@@ -1,15 +1,17 @@
 package main
+
 import (
-    "log"
-    "log/syslog"
+	"log"
+	"log/syslog"
 )
+
 func main() {
-    sysLog, err := syslog.New(syslog.LOG_SYSLOG, "systemLog.go")
-    if err != nil {
-        log.Println(err)
-        return
-    } else {
-        log.SetOutput(sysLog)
-        log.Print("Everything is fine!")
-    }
+	sysLog, err := syslog.New(syslog.LOG_SYSLOG, "systemLog.go")
+	if err != nil {
+		log.Println(err)
+		return
+	} else {
+		log.SetOutput(sysLog)
+		log.Print("Everything is fine!")
+	}
 }

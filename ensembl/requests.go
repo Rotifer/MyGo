6a6945b4115dad4ae983d72d211ff208273e0fc6
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-10-20 17:26:32 +0100

    Formatted
    Formatted requests program

--- ensembl/requests.go
+++ ensembl/requests.go
@@ -2,11 +2,11 @@ package main
 
 import "github.com/asmcos/requests"
 
-func main (){
+func main() {
 
-        resp,err := requests.Get("http://www.zhanluejia.net.cn")
-        if err != nil{
-          return
-        }
-        println(resp.Text())
+	resp, err := requests.Get("http://www.zhanluejia.net.cn")
+	if err != nil {
+		return
+	}
+	println(resp.Text())
 }

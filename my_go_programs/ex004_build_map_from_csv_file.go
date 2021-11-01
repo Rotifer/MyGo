d8a6628f66c8be179bb35555aa43d869ca663053
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-11-01 17:36:35 +0000

    Added new functions to go program

--- my_go_programs/ex004_build_map_from_csv_file.go
+++ my_go_programs/ex004_build_map_from_csv_file.go
@@ -13,15 +13,19 @@ func main() {
 	fmt.Println(len(dict))
 	key := "24954895"
 	fmt.Println(dict[key])
+	columnValues := uniqueListFromFileColumn("./data/analysis.csv", 8)
+	elementsInMap := getElementsInMap(columnValues, dict)
+	fmt.Printf("%v\n", columnValues)
+	fmt.Println("%v\n", elementsInMap)
 }
 
-func buildPubmedIDToStudyAccessionMap(filepath string) map[string]string {
+func buildPubmedIDToStudyAccessionMap(filePath string) map[string]string {
 	pubmedIdToStudyaccessionMap := make(map[string]string)
-	_, err := os.Stat(filepath)
+	_, err := os.Stat(filePath)
 	if err != nil {
 		os.Exit(1)
 	}
-	f, err := os.Open(filepath)
+	f, err := os.Open(filePath)
 	if err != nil {
 		os.Exit(1)
 	}
@@ -44,3 +48,50 @@ func buildPubmedIDToStudyAccessionMap(filepath string) map[string]string {
 	return pubmedIdToStudyaccessionMap
 }
 
+func  uniqueListFromFileColumn(filePath string, columnIndex int) []string {
+	var columnValues  []string
+	var value string
+	setMap := make(map[string]string)
+	_, err := os.Stat(filePath)
+	if err != nil {
+		os.Exit(1)
+	}
+	
+	f, err := os.Open(filePath)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = -1
+	for {
+		record, err := reader.Read()
+		// Stop at EOF.
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+		value = record[columnIndex]
+		setMap[value] = value
+	}
+
+	for k, _ := range setMap {
+		columnValues = append(columnValues, k)
+	}
+
+	return columnValues
+}
+
+
+func getElementsInMap(s []string, dict map[string]string) []string {
+	var elementsInMap []string
+	for _, el := range s {
+		if _, ok :=  dict[el]; ok {
+			elementsInMap = append(elementsInMap, el)
+		}
+	}
+	return elementsInMap
+}
